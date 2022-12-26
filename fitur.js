388dc5241f3ebd92bdf63a36ee4a6f417b6e06f9
vandueniecky <vandu.niecky14@gmail.com>
2022-12-26 15:05:41 +0700

    Update 26.12.22

--- fitur.js
+++ fitur.js
@@ -2,6 +2,25 @@ let taggOne = document.getElementById('tagg1');
 let popupOne = document.getElementById('popuptagg1');
 let taggTwo = document.getElementById('tagg2');
 let popupTwo = document.getElementById('popuptagg2')
+let taggThree = document.getElementById('tagg3');
+let popupThree = document.getElementById('popuptagg3')
+let taggFour = document.getElementById('tagg4');
+let popupFour = document.getElementById('popuptagg4')
+let taggFive = document.getElementById('tagg5');
+let popupFive = document.getElementById('popuptagg5')
+let taggSix = document.getElementById('tagg6');
+let popupSix = document.getElementById('popuptagg6')
+let taggSeven = document.getElementById('tagg7');
+let popupSeven = document.getElementById('popuptagg7')
+let taggEight = document.getElementById('tagg8');
+let popupEight = document.getElementById('popuptagg8')
+let taggNine = document.getElementById('tagg9');
+let popupNine = document.getElementById('popuptagg9')
+let taggTen = document.getElementById('tagg10');
+let popupTen = document.getElementById('popuptagg10')
+let taggEleven = document.getElementById('tagg11');
+let popupEleven = document.getElementById('popuptagg11')
+// Batas
 
 taggOne.addEventListener("mouseenter", (e) =>{
     $('#popuptagg1').show();
@@ -16,9 +35,10 @@ popupOne.addEventListener("mouseenter", () =>{
 });
 
 popupOne.addEventListener("mouseleave", () =>{
-    $('#popuptagg1').hide();
+    setTimeout(() => {
+        $('#popuptagg1').hide();
+    }, 1000);
 });
-
 // Batas
 
 taggTwo.addEventListener("mouseenter", (e) =>{
@@ -34,5 +54,154 @@ popupTwo.addEventListener("mouseenter", () =>{
 });
 
 popupTwo.addEventListener("mouseleave", () =>{
-    $('#popuptagg2').hide();
+    setTimeout(() => {
+        $('#popuptagg2').hide();
+    }, 1000);
+});
+// Batas
+
+taggThree.addEventListener("mouseenter", (e) =>{
+    $('#popuptagg3').show();
+});
+
+taggThree.addEventListener("mouseleave", (e) =>{
+    setTimeout(() => {
+        $('#popuptagg3').hide();
+    }, 1000);
+});
+// Batas
+
+taggFour.addEventListener("mouseenter", (e) =>{
+    $('#popuptagg4').show();
+});
+
+taggFour.addEventListener("mouseleave", (e) =>{
+    $('#popuptagg4').hide();
+});
+
+popupFour.addEventListener("mouseenter", () =>{
+    $('#popuptagg4').show();
+});
+
+popupFour.addEventListener("mouseleave", () =>{
+    setTimeout(() => {
+        $('#popuptagg4').hide();
+    }, 1000);
+});
+// Batas
+
+taggFive.addEventListener("mouseenter", (e) =>{
+    $('#popuptagg5').show();
+});
+
+taggFive.addEventListener("mouseleave", (e) =>{
+    $('#popuptagg5').hide();
+});
+
+popupFive.addEventListener("mouseenter", () =>{
+    $('#popuptagg5').show();
+});
+
+popupFive.addEventListener("mouseleave", () =>{
+    setTimeout(() => {
+        $('#popuptagg5').hide();
+    }, 1000);
+});
+// Batas
+
+taggSix.addEventListener("mouseenter", (e) =>{
+    $('#popuptagg6').show();
+});
+
+taggSix.addEventListener("mouseleave", (e) =>{
+    setTimeout(() => {
+        $('#popuptagg6').hide();
+    }, 1000);
+});
+// Batas
+
+taggSeven.addEventListener("mouseenter", (e) =>{
+    $('#popuptagg7').show();
+});
+
+taggSeven.addEventListener("mouseleave", (e) =>{
+    setTimeout(() => {
+        $('#popuptagg7').hide();
+    }, 1000);
+});
+// Batas
+
+taggEight.addEventListener("mouseenter", (e) =>{
+    $('#popuptagg8').show();
+});
+
+taggEight.addEventListener("mouseleave", (e) =>{
+    $('#popuptagg8').hide();
+});
+
+popupEight.addEventListener("mouseenter", () =>{
+    $('#popuptagg8').show();
+});
+
+popupEight.addEventListener("mouseleave", () =>{
+    setTimeout(() => {
+        $('#popuptagg8').hide();
+    }, 1000);
+});
+// Batas
+
+taggNine.addEventListener("mouseenter", (e) =>{
+    $('#popuptagg9').show();
+});
+
+taggNine.addEventListener("mouseleave", (e) =>{
+    $('#popuptagg9').hide();
+});
+
+popupNine.addEventListener("mouseenter", () =>{
+    $('#popuptagg9').show();
+});
+
+popupNine.addEventListener("mouseleave", () =>{
+    setTimeout(() => {
+        $('#popuptagg9').hide();
+    }, 1000);
+});
+// Batas
+
+taggTen.addEventListener("mouseenter", (e) =>{
+    $('#popuptagg10').show();
+});
+
+taggTen.addEventListener("mouseleave", (e) =>{
+    $('#popuptagg10').hide();
+});
+
+popupTen.addEventListener("mouseenter", () =>{
+    $('#popuptagg8').show();
+});
+
+popupTen.addEventListener("mouseleave", () =>{
+    setTimeout(() => {
+        $('#popuptagg10').hide();
+    }, 1000);
+});
+// Batas
+
+taggEleven.addEventListener("mouseenter", (e) =>{
+    $('#popuptagg11').show();
+});
+
+taggEleven.addEventListener("mouseleave", (e) =>{
+    $('#popuptagg11').hide();
+});
+
+popupEleven.addEventListener("mouseenter", () =>{
+    $('#popuptagg11').show();
+});
+
+popupEleven.addEventListener("mouseleave", () =>{
+    setTimeout(() => {
+        $('#popuptagg11').hide();
+    }, 1000);
 });
